b81c3fa724dee4734df917d467c38b7b35da4f37
TomasTNunes <tomastrindadenunes@outlook.com>
2025-03-03 13:54:35 +0100

    chore: comment reply_embed

--- src/assets/replies/reply_embed.py
+++ src/assets/replies/reply_embed.py
@@ -2,10 +2,13 @@ import discord
 from discord import Embed
 
 def error_embed(text: str):
+    """Returns an red embed with received message."""
     return Embed(color = discord.Colour.red(), description=text)
 
 def warning_embed(text: str):
+    """Returns an orange embed with received message."""
     return Embed(color = discord.Colour.orange(), description=text)
 
 def success_embed(text: str):
+    """Returns an green embed with received message."""
     return Embed(color = discord.Colour.green(), description=text)
