d232582832cb7bd458812a0c1be9e530ceb59fd2
TomasTNunes <tomastrindadenunes@outlook.com>
2025-03-04 14:22:09 +0100

    feat: playlist add / command and buttons

--- src/assets/music/musicplayerview.py
+++ src/assets/music/musicplayerview.py
@@ -5,6 +5,7 @@ from discord.ui import Button, View
 from lavalink.events import QueueEndEvent
 import random
 from assets.replies.reply_embed import error_embed, success_embed, warning_embed
+from assets.music.playlistbutton import PlaylistButton
 
 class MusicPlayerView(View):
     """Class to control View with buttons for music message. Each music message has its own View instance."""
@@ -35,7 +36,7 @@ class MusicPlayerView(View):
         - Shuffle
         - AutoPlay
         - Connect/Disconnect
-        - Custom Playslists (in the future)
+        - Custom Playslists
 
         NOTE: When using persistent views, Discord identifies buttons using custom_id across all active views, hence 
         across all MusicPlayerView instances for all guilds. If multiple instances of MusicPlayerView exist
@@ -177,13 +178,47 @@ class MusicPlayerView(View):
         stop.callback = self.stop_callback
         self.add_item(stop)
 
+        # Playlists Buttons
+        playlists = self.cog.get_guild_music_data(self.guild.id).get("playlists", None)
+        if playlists:
+            for i, pl_name in enumerate(playlists):
+                # Get playlist emoji if exists
+                if playlists[pl_name].get('emoji'):
+                    if playlists[pl_name].get('emoji').get('unicode'):
+                        playlist_emoji = playlists[pl_name].get('emoji').get('name')
+                    else:
+                        playlist_emoji = PartialEmoji(
+                            name=playlists[pl_name].get('emoji').get('name'),
+                            id = playlists[pl_name].get('emoji').get('id')
+                        )
+                else:
+                    playlist_emoji = None
+
+                # Create playlist button
+                playlist_button = PlaylistButton(
+                    url=playlists[pl_name].get('url'),
+                    style=discord.ButtonStyle.grey,
+                    emoji=playlist_emoji,
+                    label=playlists[pl_name].get('button_name', None),
+                    custom_id=f"vibebot_playlist_{pl_name}_{self.guild.id}",
+                    row=2 if i<=4 else 3,
+                    disabled=False
+                )
+                self.add_item(playlist_button)
+        
         # Connect/Disconnect button
+        if not playlists or len(playlists) == 0:
+            connect_row = 2
+        elif len(playlists) <= 5:
+            connect_row = 3
+        else:
+            connect_row = 4
         connect = Button(
             style=discord.ButtonStyle.red if is_connected else discord.ButtonStyle.green,
             emoji=PartialEmoji(name="vibebot_disconnect", id=1344955759284191233) if is_connected else PartialEmoji(name="vibebot_connect", id=1344955706322976808),
             label="Disconnect Bot" if is_connected else "Connect Bot",
             custom_id=f"vibebot_connect_{self.guild.id}",
-            row=2,
+            row=connect_row,
             disabled=False
         )
         connect.callback = self.connect_callback
@@ -202,7 +237,7 @@ class MusicPlayerView(View):
         custom_id = interaction.data.get("custom_id").split("_")[1]
 
         # If the button is connect, than the bot should join the voice channel if possible
-        should_connect = True if custom_id == "connect" else False
+        should_connect = True if custom_id == "connect" or custom_id == "playlist" else False
 
         # If the button is previous, resume, skip, loop, shuffle or stop, than the bot should be playing
         should_bePlaying = True if custom_id in ["previous", "resume", "next", "shuffle", "stop"] else False
@@ -296,16 +331,11 @@ class MusicPlayerView(View):
         NOTE: When bot is connected without music playing the Resume button is shown. When in this state, clicking Resume
         will send warning that not music is playing.
         """
-        # Get guild player
-        player = self.cog.lavalink.player_manager.get(self.guild.id)
-
-        # Check if player is playing
-        if not player.is_playing:
-            await interaction.response.send_message(embed=error_embed("No music is playing."), ephemeral=True)
-            return
-
         # Defer the interaction
         await interaction.response.defer()
+
+        # Get guild player
+        player = self.cog.lavalink.player_manager.get(self.guild.id)
         
         # Check if player is paused
         if player.paused:
@@ -355,7 +385,7 @@ class MusicPlayerView(View):
 
         # Check if volume is already minimum (0)
         if volume == 200:
-            await interaction.response.send_message("Volume is already at maximum (200%).", ephemeral=True)
+            await interaction.response.send_message(embed=error_embed("Volume is already at maximum (200%)."), ephemeral=True)
             return
 
         # Defer the interaction
