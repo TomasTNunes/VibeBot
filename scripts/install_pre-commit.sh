0de93b823470361da8f123b9c9e3ea32c3f5e74d
TomasTNunes <tomastrindadenunes@outlook.com>
2025-03-05 11:31:53 +0100

    build: add pre-commit check in local

---
+++ scripts/install_pre-commit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# after cloning/forking repository, and installing python requirements from requirements.txt
+# run this script to install pre-commit hooks
+pre-commit install --install-hooks
